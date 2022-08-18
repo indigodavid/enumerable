1e85d36d7246da0550c6aea25469f2264f62ec84
David Vera Castillo <indigodavid@gmail.com>
2022-08-18 17:50:38 -0500

    Add class MyList basic structure

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,6 +1,50 @@
 require_relative 'enumerable/version'
 
-module RubyTemplate
+module Enumerable
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class MyList
+    @list = []
+
+    def initialize (*elements)
+      @list = elements
+    end
+
+    def all?
+      # all method
+      
+    end
+
+    def any?
+      # any method
+      
+    end
+
+    def filter
+      # filter method
+      
+    end
+  end
+
+  # Create our list
+  list = MyList.new(1, 2, 3, 4)
+  # => #<MyList: @list=[1, 2, 3, 4]>
+
+  # Test #all?
+  list.all? {|e| e < 5}
+  # => true
+  list.all? {|e| e > 5}
+  # => false
+
+  # Test #any?
+  list.any? {|e| e == 2}
+  # => true
+  list.any? {|e| e == 5}
+  # => false
+
+  # Test #filter
+  list.filter {|e| e.even?}
+  # => [2, 4]
 end
+
+
